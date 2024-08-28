f159d9a644e4987f0a2f6f0d24988571466e2c90
nonnysteph <nonsos595@gmail.com>
2024-08-28 13:04:28 +0000

    feat: initiated the project

---
+++ contracts/Hello_swtr.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+contract Swisstronik {
+    string private message;
+    function initialize(string memory _message) public {
+        message = _message;
+    }
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
+    function getMessage() public view returns(string memory) {
+        return message;
+    }
+}
